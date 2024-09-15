dbf83910d57f5d5963ff748295166f98133ac0d9
TARSICIO RAMOS LEMUS <tramos@TARSICIOs-MacBook-Air.local>
2024-09-15 14:02:55 -0600

    se requirio el p aquete morgan en index.js principal

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const engine = require('ejs-mate');
 const path = require('path');
+const morgan = require('morgan');
 
 const app = express();
 
@@ -10,6 +11,9 @@ app.set('views', path.join(__dirname,'views'));
 app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 app.set('port', process.env.PORT||3000);
+
+//Middlewares
+app.use(morgan('dev'));
 
 //Routes
 app.use('/', require('./routes/index'));
