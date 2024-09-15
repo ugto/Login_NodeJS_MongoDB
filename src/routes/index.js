e451b50d9e0053fbbb0b60cffcb0df552127535b
TARSICIO RAMOS LEMUS <tramos@TARSICIOs-MacBook-Air.local>
2024-09-15 13:06:15 -0600

    cración del archivo roites/index.js que será el que almacena las rutas que se van a implememtar en el sistema, donse se crearon las primeras rutas como: signin, signup, profile y /

---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/',(req, res, next)=>{});
+router.get('/signup',(req, res, next)=>{});
+router.post('/signup',(req, res, next)=>{});
+router.get('/signin',(req, res, next)=>{});
+router.post('/signin',(req, res, next)=>{});
+router.get('/profile',(req, res, next)=>{});
+
+module.exports= routes;
